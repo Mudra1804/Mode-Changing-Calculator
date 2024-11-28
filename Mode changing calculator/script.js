437ff7fa1675dd161734aa1a5040c7c7af08a28d
Mudra1804 <134253557+Mudra1804@users.noreply.github.com>
2024-11-28 10:36:19 +0530

    script.js

--- Mode changing calculator/script.js
+++ Mode changing calculator/script.js
@@ -1,31 +1,28 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach((item) => {
-    item.onclick = () => {
-        if (item.id === "clear") {
+buttons.forEach((button) => {
+    button.onclick = () => {
+        if (button.id === "clear") {
             display.innerText = "";
-        } else if (item.id === "backspace") {
-            let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length - 1);
-        } else if (display.innerText !== "" && item.id === "equal") {
-            display.innerText = eval(display.innerText);
-        } else if (display.innerText === "" && item.id === "equal") {
-            display.innerText = "Empty!";
-            setTimeout(() => (display.innerText = ""), 2000);
+        } else if (button.id === "backspace") {
+            display.innerText = display.innerText.slice(0, -1);
+        } else if (button.id === "equal") {
+            try {
+                display.innerText = eval(display.innerText.replace(/×/g, '*').replace(/÷/g, '/'));
+            } catch {
+                display.innerText = "Error!";
+                setTimeout(() => (display.innerText = ""), 2000);
+            }
         } else {
-            display.innerText += item.innerText;
+            display.innerText += button.innerText;
         }
     };
 });
 
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
 
 themeToggleBtn.onclick = () => {
     calculator.classList.toggle("dark");
-    themeToggleBtn.classList.toggle("active");
-    isDark = !isDark;
 };
